649d55e7d48668e9caccd5d196017ead6286f71b
nathaniacalista01 <nathania.calista01@gmail.com>
2023-10-15 19:48:25 +0700

    fix: tree minimax
    Co-authored-by: Alexander Jason <alexanderjason526@gmail.com>
    Co-authored-by: Antonio Natthan <natthankrish@gmail.com>

--- src/BotMoves/MiniMaxABAlgo.java
+++ src/BotMoves/MiniMaxABAlgo.java
@@ -71,8 +71,17 @@ public class MiniMaxABAlgo implements Algorithm{
     };
 
     public int calculateObjective(char[][] boardMap){
-//        Iterasi seluruh map buat cari total point dia berapa
-        return 0;
+        int point = 0;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (boardMap[i][j] == 'O') {
+                    point++;
+                } else if (boardMap[i][j] == 'X') {
+                    point--;
+                }
+            }
+        }
+        return point;
     }
 
     public ArrayList<int[]> searchAdjacent(char[][] boardMap) {
@@ -91,7 +100,7 @@ public class MiniMaxABAlgo implements Algorithm{
     public int[] move(char[][] boardMap) {
         this.root = new Tree(boardMap);
 
-        processTree(boardMap, true, 0, 3, -999, 999);
+        processTree(boardMap, true, 0, 2, -999, 999);
 
         this.root.printTree();
 
@@ -109,26 +118,37 @@ public class MiniMaxABAlgo implements Algorithm{
                 }
 
                 changeState(state, isBot, point[0], point[1]);
-                for(int i = 0; i < 8; i++) {
-                    for (int p = 0; p < depth; p++) {
-                        System.out.print("  ");
-                    }
-                    System.out.print("| ");
-                    for(int j = 0; j < 8; j++) {
-                        System.out.print(state[i][j] + " ");
-                    }
-                    System.out.println();
-                }
-                System.out.println("-------------------------");
+//                for(int i = 0; i < 8; i++) {
+//                    for (int p = 0; p < depth; p++) {
+//                        System.out.print("  ");
+//                    }
+//                    System.out.print("| ");
+//                    for(int j = 0; j < 8; j++) {
+//                        System.out.print(state[i][j] + " ");
+//                    }
+//                    System.out.println();
+//                }
+//                System.out.println("-------------------------");
                 int val = processTree(state, !isBot, depth+1, leftround-1, alpha, beta);
+
                 if (isBot && val > alpha) {
                     alpha = val;
                 } else if (!isBot && val < beta) {
                     beta = val;
                 }
             }
+            for (int p = 0; p < depth; p++) {
+                System.out.print("  ");
+            }
+            System.out.print(isBot + "| ");
+            System.out.println(isBot ? alpha : beta);
             return isBot ? alpha : beta;
         } else { //
+            for (int p = 0; p < depth; p++) {
+                System.out.print("  ");
+            }
+            System.out.print(isBot + "| ");
+            System.out.println(calculateObjective(boardMap));
             return calculateObjective(boardMap);
         }
     }
